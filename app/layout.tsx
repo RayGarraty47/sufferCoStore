6269d7ebb6b3fdda8ee82fee471b252ef311e550
RayGarraty47 <153393337+RayGarraty47@users.noreply.github.com>
2024-01-13 15:39:57 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
-import type { Metadata } from 'next'
 import { Exo_2 } from 'next/font/google'
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 
 import './globals.css'
 
-import Footer from '@/components/footer'
-import Navbar from '@/components/navbar'
-import ModalProvider from '@/providers/modal-provider'
-import ToastProvider from '@/providers/toast-provider'
-
 const font = Exo_2({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Store',
-  description: 'Store',
+  description: 'Store - The place for all your purchases.',
 }
 
 export default function RootLayout({
@@ -23,12 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <ModalProvider />
         <ToastProvider />
+        <ModalProvider />
         <Navbar />
         {children}
-          <Footer />
-        </body>
+        <Footer />
+      </body>
     </html>
   )
 }
+
